75db78bbc963095a473dee12d915ed3ad0ec8858
almartin82 <almartin@gmail.com>
2015-04-22 16:52:19 -0400

    generic fetch function (wrapper around different program calls)

---
+++ R/fetch_nj_assess.R
@@ -0,0 +1,94 @@
+#' @title determine if a year/grade pairing can be downloaded from the state website
+#' 
+#' @description
+#' \code{valid_call} returns a boolean value indicating if a given year/grade pairing is
+#' valid for assessment data
+#' @inheritParams fetch_njask
+#' @export
+
+valid_call <- function(year, grade) {
+  #data for 2015 school year doesn't exist yet
+  #common core transition started in 2015 (njask is no more)
+  if(year > 2014) {
+    valid_call <- FALSE
+  #assessment coverage 3:8 from 2006 on.
+  #NJASK fully implemented in 2008
+  } else if(year >= 2006) {
+    valid_call <- grade %in% c(3:8, 11)
+  } else if (year >= 2004) {
+    valid_call <- grade %in% c(3, 4, 8, 11)
+  } else if (year < 2004) {
+    valid_call <- FALSE
+  }
+  
+  return(valid_call)
+}
+
+
+
+#' @title call the correct \code{fetch} function for normal assessment years
+#' 
+#' @description for 2008-2014, this function will grab the NJASK for gr 3-8, and HSPA
+#' for grade 11
+#' @inheritParams fetch_njask
+#' @export
+
+standard_assess <- function(year, grade) {
+  if(grade %in% c(3:8)) {
+    assess_data <- fetch_njask(year, grade)
+  } else if (grade == 11) {
+    assess_data <- fetch_hspa(year) 
+  }
+  
+  return(assess_data)
+} 
+
+
+
+#' @title a simplified interface into NJ assessment data
+#' 
+#' @description this is the workhorse function.  given a year and a grade (valid years are 2004-present), 
+#' \code{fetch_nj_assess} will call the appropriate function, process the raw 
+#' text file, and return a data frame.  \code{fetch_nj_assess} is a wrapper around 
+#' all the individual subject functions (NJASK, HSPA, etc.), abstracting away the 
+#' complexity of finding the right location/file layout.
+#' @inheritParams fetch_njask
+#' @export
+
+fetch_nj_assess <- function(year, grade) {
+  #only allow valid calls
+  valid_call(year, grade) %>%
+    ensurer::ensure_that(
+      all(.) ~ "invalid grade/year parameter passed")
+  
+  #everything post 2008 has the same grade coverage
+  if (year >= 2008) {
+    assess_data <- standard_assess(year, grade)
+    
+  #2006 and 2007: NJASK 3rd-7th, GEPA 8th, HSPA 11th
+  } else if (year %in% c(2006, 2007)) {
+    if (grade %in% c(3:7)) {
+      assess_data <- standard_assess(year, grade)  
+    } else if (grade == 8) {
+      assess_data <- fetch_gepa(year)
+    } else if (grade == 11) {
+      assess_data <- fetch_hspa(year)
+    }
+    
+  #2004 and 2005:  NJASK 3rd & 4th, GEPA 8th, HSPA 11th
+  } else if (year %in% c(2004, 2005)) {
+    if (grade %in% c(3:4)) {
+      assess_data <- standard_assess(year, grade)  
+    } else if (grade == 8) {
+      assess_data <- fetch_gepa(year)
+    } else if (grade == 11) {
+      assess_data <- fetch_hspa(year)
+    }
+  
+  } else {
+    #if we ever reached this block, there's a problem with our `valid_call()` function
+    stop("unable to match your grade/year parameters to the appropriate function.")
+  }
+ 
+  return(assess_data)
+}
---
+++ tests/testthat/test_fetch_nj_assess.R
@@ -0,0 +1,75 @@
+context("functions in fetch_nj_assess.R")
+
+
+test_that("valid_call correctly identifies status of years/grade pairs", {
+
+  expect_true(valid_call(2014, 8))
+  expect_false(valid_call(2014, 12))
+  
+  expect_true(valid_call(2007,8))
+  expect_false(valid_call(2005, 5))  
+})
+
+
+test_that("standard_assess correctly calls data for 2014",{
+  
+  hspa_ex <-standard_assess(2014, 11)
+  expect_equal(nrow(hspa_ex), 371)
+  expect_equal(ncol(hspa_ex), 559)
+  expect_equal(
+    sum(hspa_ex$GENERAL_EDUCATION_Number_Enrolled_LAL, na.rm = TRUE), 211258
+  )
+  expect_equal(
+    sum(hspa_ex$GENERAL_EDUCATION_LANGUAGE_ARTS_Scale_Score_Mean, na.rm = TRUE), 
+    86826.7, tolerance = 0.01 
+  )
+  
+  njask_ex <-standard_assess(2014, 7)
+  expect_equal(nrow(njask_ex), 777)
+  expect_equal(ncol(njask_ex), 551)
+  expect_equal(
+    sum(njask_ex$GENERAL_EDUCATION_Number_Enrolled_ELA, na.rm = TRUE), 269837
+  )
+  expect_equal(
+    sum(njask_ex$GENERAL_EDUCATION_LANGUAGE_ARTS_Scale_Score_Mean, na.rm = TRUE),
+    157404.5, tolerance = 0.01 
+  )
+  
+})
+
+
+test_that("fetch_nj_assess returns correct output for a variety of calls", {
+  
+  #2014 njask
+  njask_14 <- fetch_nj_assess(2014, 6)
+  expect_equal(nrow(njask_14), 881)
+  expect_equal(ncol(njask_14), 551)
+  expect_equal(
+    sum(njask_14$GENERAL_EDUCATION_Number_Enrolled_ELA, na.rm = TRUE), 247086
+  )
+  
+  #2014 hspa
+  hspa_14 <- fetch_nj_assess(2014, 11)
+  expect_equal(nrow(hspa_14), 371)
+  expect_equal(ncol(hspa_14), 559)
+  expect_equal(
+    sum(hspa_14$GENERAL_EDUCATION_Number_Enrolled_LAL, na.rm = TRUE), 211258
+  )
+  
+  #2007 gepa
+  gepa_07 <- fetch_nj_assess(2007, 11)
+  expect_equal(nrow(gepa_07), 681)
+  expect_equal(ncol(gepa_07), 527)
+  expect_equal(
+    sum(gepa_07$GENERAL_EDUCATION_Number_Enrolled, na.rm = TRUE), 410704
+  )
+  
+  #2004 gr3
+  njask_04 <- fetch_nj_assess(2004, 3)
+  expect_equal(nrow(njask_04), 1956)
+  expect_equal(ncol(njask_04), 362)
+  expect_equal(
+    sum(njask_04$GENERAL_EDUCATION_Number_Enrolled, na.rm = TRUE), 418669
+  )
+
+})
