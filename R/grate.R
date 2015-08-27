feee93a842b31face587896112ef72c0df72be33
almartin82 <almartin@gmail.com>
2015-08-27 17:58:11 -0400

    parse and tidy, almost complete

--- R/grate.R
+++ R/grate.R
@@ -90,11 +90,13 @@ get_raw_grate <- function(end_year, calc_type = '4 year') {
     
     #extensions changed
     if (end_year >= 2009) {
-      grade <- readxl::read_excel(paste0(unzip_loc, '\\', grate_files$Name[1]))
+      grate <- readxl::read_excel(paste0(unzip_loc, '\\', grate_files$Name[1]))
     } else {
       grate <- readr::read_csv(file = paste0(unzip_loc, '\\', grate_files$Name[1]))
     }
-    
+   
+    grate$methodology <- 'grad_count'
+ 
     #clean up
     closeAllConnections()
     unlink(unzip_loc, recursive = TRUE)
@@ -120,15 +122,15 @@ get_raw_grate <- function(end_year, calc_type = '4 year') {
 
 process_grate <- function(df, end_year) {
   #clean up names
-  names(df)[names(df) %in% c('COUNTY', 'CO', 'CO_NAME')] <- 'county_name'
-  names(df)[names(df) %in% c('DISTRICT', 'DIST', 'DIS_NAME')] <- 'district_name'
-  names(df)[names(df) %in% c('SCHOOL', 'SCH', 'SCH_NAME')] <- 'school_name'
-  names(df)[names(df) %in% c('PROG_CODE')] <- 'program_code'
-  names(df)[names(df) %in% c('PROGNAME', 'PROG')] <- 'program_name'
+  names(df)[names(df) %in% c('COUNTY', 'CO', 'CO NAME', 'CO_NAME')] <- 'county_name'
+  names(df)[names(df) %in% c('DISTRICT', 'DIST', 'DIST NAME', 'DIS_NAME')] <- 'district_name'
+  names(df)[names(df) %in% c('SCHOOL', 'SCH', 'SCH NAME', 'SCH_NAME')] <- 'school_name'
+  names(df)[names(df) %in% c('PROG_CODE', 'PROG CODE')] <- 'program_code'
+  names(df)[names(df) %in% c('PROGNAME', 'PROG', 'PROG NAME')] <- 'program_name'
   
-  names(df)[names(df) %in% c('COUNTY_CODE', 'County')] <- 'county_id'
-  names(df)[names(df) %in% c('DISTRICT_CODE', 'District')] <- 'district_id'
-  names(df)[names(df) %in% c('SCHOOL_CODE', 'School')] <- 'school_id'
+  names(df)[names(df) %in% c('COUNTY_CODE', 'CO CODE', 'County')] <- 'county_id'
+  names(df)[names(df) %in% c('DISTRICT_CODE', 'DIST CODE', 'District')] <- 'district_id'
+  names(df)[names(df) %in% c('SCHOOL_CODE', 'SCH CODE', 'School')] <- 'school_id'
   
   #errata
   names(df)[names(df) %in% c('HISP_MALE')] <- 'hisp_m'  
@@ -150,11 +152,28 @@ process_grate <- function(df, end_year) {
   
   names(df) <- names(df) %>% tolower()
 
+
+  numeric_cols <- c("rowtotal", "female", "male",
+    "white", "black", "hispanic", "american_indian", 
+    "asian", "pacific_islander", "multiracial",
+    "white_m", "white_f", "black_m", "black_f", 
+    "hisp_m", "hisp_f", "nat_am_m", "nat_am_f", 
+    "asian_m", "asian_f", "hwn_nat_m", "hwn_nat_f", 
+    "multiracial_m", "multiracial_f", 
+    "instate", "outstate"
+  )
+  
+  for (i in numeric_cols) {
+    if (i %in% names(df)) {
+      df[, i] <- df %>% dplyr::select_(i) %>% unlist() %>% as.numeric()
+    }
+  }
+  
   #county distr sch codes
   if (end_year <= 2010) {
     
     #county_id and county_name
-    int_matrix <- stringr::str_split_fixed(df$county_name, "-", 2)    
+    int_matrix <- stringr::str_split_fixed(df$county_name, "-", 2)
     df$county_id <- int_matrix[, 1]
     df$county_name <- int_matrix[, 2]
     
@@ -170,30 +189,18 @@ process_grate <- function(df, end_year) {
  
   }
   
-  #missing program names
-  
-  if (end_year >= 2013 ) {
+  df_coltypes <- sapply(df, class) %>% unname()
+
+  for (i in 1:ncol(df)) {
+    if (df_coltypes[i] == 'character') {
+      df[, i] <- sapply(df[, i], trimws, which = 'both')
+    }
   }
   
-  clean_names <- c(
-    "county_id", "county_name", 
-    "district_id", "district_name", 
-    "school_id", "school_name", 
-    "level", 
-    "grad_cohort", "year_reported", "methodology", "time_window",
-    "program_name", "program_code", 
-    "white_m", "white_f", 
-    "black_m", "black_f", 
-    "hisp_m", "hisp_f", 
-    "nat_am_m", "nat_am_f", 
-    "asian_m", "asian_f", 
-    "hwn_nat_m", "hwn_nat_f",
-    "2_more_m", "2_more_f", 
-    "rowtotal", 
-    "instate", "outstate", 
-    "subgroup",
-    "grad_rate", "cohort_count", "graduated_count"
-  )
+  #missing program names
+  #TO-DO
+  
+  
 
   return(df)
 }
@@ -210,13 +217,167 @@ process_grate <- function(df, end_year) {
 
 
 
-tidy_grate <- function(df) {
+tidy_grate <- function(df, end_year) {
   
+  grate_col <- function(col_name, nj_df) {
+    nj_df <- nj_df %>% as.data.frame(stringsAsFactors = FALSE)
+    
+    mask <- grepl(col_name, names(nj_df))
+    if (sum(mask) > 1) stop("tidying grate data matched more than one column")
+    
+    if (all(mask == FALSE)) {
+      out <- rep(NA, nrow(nj_df))
+    } else {
+      out <- nj_df[, mask, drop = TRUE]
+    }
+    return(out)
+  }
+  
+  
+  tidy_old_format <- function(sch_subset) {
+
+    #constants
+    constant_df <- data.frame(
+      county_id = grate_col('county_id', sch_subset) %>% unique(),
+      county_name = grate_col('county_name', sch_subset) %>% unique(),
+
+      district_id = grate_col('district_id', sch_subset) %>% unique(),
+      district_name = grate_col('district_name', sch_subset) %>% unique(),
+
+      school_id = grate_col('school_id', sch_subset) %>% unique(),
+      school_name = grate_col('school_name', sch_subset) %>% unique(),
+
+      grad_cohort = grate_col('grad_cohort', sch_subset) %>% unique(),
+      year_reported = grate_col('year_reported', sch_subset) %>% unique(),
+      methodology = grate_col('methodology', sch_subset) %>% unique(),
+      time_window = grate_col('time_window', sch_subset) %>% unique(),
+
+      stringsAsFactors = FALSE
+    )
+    
+    #build composites
+    sch_subset$white <- sch_subset %$% add(white_m, white_f)
+    sch_subset$black <- sch_subset %$% add(black_m, black_f)
+    sch_subset$hispanic <- sch_subset %$% add(hisp_m, hisp_f)
+    sch_subset$american_indian <- sch_subset %$% add(nat_am_m, nat_am_f)
+    sch_subset$asian <- sch_subset %$% add(asian_m, asian_f)
+    
+    #force NA for some subgroups if not present
+    sch_subset$hwn_nat_m <- grate_col('hwn_nat_m', sch_subset)
+    sch_subset$hwn_nat_f <- grate_col('hwn_nat_f', sch_subset)
+    sch_subset$pacific_islander <- sch_subset %$% add(hwn_nat_m, hwn_nat_f)
+    
+    sch_subset$multiracial_m <- grate_col('multiracial_m', sch_subset)
+    sch_subset$multiracial_f <- grate_col('multiracial_f', sch_subset)
+    sch_subset$multiracial <- sch_subset %$% add(multiracial_m, multiracial_f)
+    
+    sch_subset$female <- rowSums(
+      cbind(
+        sch_subset$white_f, sch_subset$black_f, sch_subset$hisp_f, 
+        sch_subset$nat_am_f, sch_subset$asian_f, sch_subset$hwn_nat_f, 
+        sch_subset$multiracial_f
+      ), na.rm = TRUE)
+    sch_subset$male <- rowSums(
+      cbind(
+        sch_subset$white_m, sch_subset$black_m, sch_subset$hisp_m, 
+        sch_subset$nat_am_m, sch_subset$asian_m, sch_subset$hwn_nat_m, 
+        sch_subset$multiracial_m
+      ), na.rm = TRUE)
+    
+    #force code if missing
+    sch_subset$program_code <- grate_col('program_code', sch_subset)
+    
+    #rowtotal is actually total population
+    names(sch_subset)[names(sch_subset) == 'rowtotal'] <- 'total_population'
+    
+    to_tidy <- c(
+      "program_name", "program_code",
+      
+      "total_population",
+      "female", "male",
+      
+      "white", "black", "hispanic", "american_indian", 
+      "asian", "pacific_islander", "multiracial",
+      
+      "white_m", "white_f", 
+      "black_m", "black_f", 
+      "hisp_m", "hisp_f", 
+      "nat_am_m", "nat_am_f", 
+      "asian_m", "asian_f", 
+      "hwn_nat_m", "hwn_nat_f", 
+      "multiracial_m", "multiracial_f", 
+      
+      "instate", "outstate"
+    )
+    col_mask <- names(sch_subset) %in% to_tidy
+    row_mask <- sch_subset$program_name == 'Total'
+    
+    sch_to_tidy <- sch_subset[, col_mask]
+    sch_to_tidy <- sch_to_tidy[to_tidy]
+    
+    sch_programs <- sch_to_tidy[!row_mask,]
+    sch_total <- sch_to_tidy[row_mask, ]
+    
+    old_tidy_list <- list()
+  
+
+    #all the subgroups
+    for (j in to_tidy[3:26]) {
+      to_pivot <- sch_to_tidy[ , c(to_tidy[1:2], j)]
+      sub_long <- reshape2::melt(to_pivot, id.vars = c('program_name', 'program_code'))
+      sub_long$variable <- as.character(sub_long$variable)
+      names(sub_long)[names(sub_long) == 'variable'] <- 'group'
+      names(sub_long)[names(sub_long) == 'value'] <- 'outcome_count'
+      sub_long$num_grad <- sub_long[sub_long$program_name == 'Total', 'outcome_count']
+      sub_long$postgrad_grad <- ifelse(sub_long$program_name == 'Total', 'grad', 'postgrad')
+      sub_long$grad_rate <- NA
+     
+      old_tidy_list[[j]] <- cbind(constant_df, sub_long) 
+    }
+    
+    out <- dplyr::rbind_all(old_tidy_list)
+    
+    return(out)
+  }
+
+
+  if (end_year < 2011) {
+    #iterate over the sch/district totals
+    df$iter_key <- paste0(df$county_id, '@', df$district_id, '@', df$school_id)
+    sch_list <- list()
+    
+    for (i in df$iter_key %>% unique()) {
+      this_sch <- df %>% dplyr::filter(iter_key == i)
+      sch_list[[i]] <- tidy_old_format(this_sch)
+    }
+      
+    out <- dplyr::rbind_all(sch_list)
+  }
+    
+  return(out)
+}
+
+
+
+
+scratch <- function() {
+  
+  
+  
+  sch_subset <- process_grate(hs_list[[2003]], 2003) %>% filter(school_name == 'ATLANTIC CITY HIGH') %>% as.data.frame()
+
+    
   hs_list <- list()
   for (i in c(1998:2014)) {
     hs_list[[i]] <- get_raw_grate(i)
   }
 
+  
+  clean_hs_list <- list()
+  for (i in c(1998:2014)) {
+    print(i)
+    clean_hs_list[[i]] <- process_grate(hs_list[[i]], i) %>% tidy_grate(., i)
+  }
 
     
   for (i in c(1998:2014)) {
@@ -250,13 +411,6 @@ tidy_grate <- function(df) {
   }
 
 
-  
-    
 }
-
-
-
-
-
 
 
