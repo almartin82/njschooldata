38df72df3619522e559f344447408aa8d5cb819f
almartin82 <almartin@gmail.com>
2016-02-04 12:10:34 -0500

    handle bad rows at end of file

--- tests/testthat/test_parcc.R
+++ tests/testthat/test_parcc.R
@@ -4,5 +4,12 @@ testthat("get_raw_parcc gets data file", {
   
   p <- get_raw_parcc(2015, 4, 'ela')
   
+  expect_equal(dim(p), c(22248, 18))
+})
+
+testthat("fetch_parcc gets and processes data file", {
   
+  p <- fetch_parcc(2015, 4, 'math')
+  
+  expect_equal(dim(p), c(22324, 22))
 })
