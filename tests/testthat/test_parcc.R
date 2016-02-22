14e2e3ad2ac5e81c137f6d43d861a9b44cd8d52c
almartin82 <almartin@gmail.com>
2016-02-22 16:12:22 -0500

    fetch_parcc can handle subject tests

--- tests/testthat/test_parcc.R
+++ tests/testthat/test_parcc.R
@@ -1,12 +1,13 @@
 context("parcc functions")
 
-testthat("get_raw_parcc gets data file", {
+test_that("get_raw_parcc gets data file", {
   
   p <- get_raw_parcc(2015, 4, 'ela')
   expect_equal(dim(p), c(22248, 18))
 })
 
-testthat("fetch_parcc gets and processes data file", {
+
+test_that("fetch_parcc gets and processes data file", {
   
   p <- fetch_parcc(2015, 4, 'math')
   expect_equal(dim(p), c(22324, 22))
@@ -14,3 +15,4 @@ testthat("fetch_parcc gets and processes data file", {
   p <- fetch_parcc(2015, 4, 'math', tidy = TRUE)
   expect_equal(dim(p), c(22324, 22))
 })
+
