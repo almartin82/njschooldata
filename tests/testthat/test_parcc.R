fe6e4b70a31eb4b889b24f13fa727597091615e7
zmaher <zachary.k.maher@gmail.com>
2016-03-23 16:51:26 -0400

    Adding some new PARCC tests

--- tests/testthat/test_parcc.R
+++ tests/testthat/test_parcc.R
@@ -4,15 +4,32 @@ test_that("get_raw_parcc gets data file", {
   
   p <- get_raw_parcc(2015, 4, 'ela')
   expect_equal(dim(p), c(22248, 18))
+  
+  expect_error(fetch_parcc(2015, 10, 'math'))
+  
 })
 
 
-test_that("fetch_parcc gets and processes data file", {
+test_that("fetch_parcc gets data file", {
   
   p <- fetch_parcc(2015, 4, 'math')
   expect_equal(dim(p), c(22324, 22))
   
+  p <- fetch_parcc(2015, 'GEO', 'math')
+  expect_equal(dim(p), c(12625, 22))
+  
+})
+
+
+test_that("fetch_parcc processes data file", {
+  
+  p <- fetch_parcc(2015, 8, 'ela', tidy = FALSE)
+  expect_equal(colnames(p)[3], "district_code")
+  
   p <- fetch_parcc(2015, 4, 'math', tidy = TRUE)
   expect_equal(dim(p), c(22324, 22))
+  expect_true("black" %in% p$subgroup)
+  expect_false("GENDER" %in% p$subgroup)
+  
 })
 
