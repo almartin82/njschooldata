edf6d7c993e8690290219096c1b77c334be86f83
almartin82 <almartin@gmail.com>
2016-10-23 16:51:35 -0400

    split into dist sch groups

--- R/peer_percentiles.R
+++ R/peer_percentiles.R
@@ -48,20 +48,42 @@ peer_percentile_pipe <- . %>%
   dplyr::select(-count_proficient_dummy, -count_scale_dummy)
 
 
-
 calc_nj_percentiles <- function(tidy_df) {
   
   #split out: districts
+  tidy_df$temp_rn <- seq(1:nrow(tidy_df))
+  
+  tidy_df <- tidy_df %>% 
+    dplyr::mutate(
+      is_sch = !school_code == '',
+      has_dist = !district_code == '',
+      is_dist = has_dist & !is_sch
+    ) %>%
+    dplyr::select(-has_dist)
   
   #split out: schools
+  sch_df <- tidy_df %>%
+    dplyr::filter(is_sch == TRUE) %>%
+    dplyr::select(-is_sch, -is_dist)
+    
+  #split out: districts
+  dist_df <- tidy_df %>%
+    dplyr::filter(is_dist == TRUE) %>%
+    dplyr::select(-is_sch, -is_dist)
   
-  #split out: everything else
+  #split out: the leftovers
+  leftover_df <- tidy_df %>%
+    dplyr::filter(
+      !temp_rn %in% c(sch_df$temp_rn, dist_df$temp_rn)
+    )
   
   #calc scale and prof peer percentiles, districts
   
   #calc scale and prof peer percentiles, schools
   
   #put everything back together and return
+  
+  
 }
 
 #scratch, for testing (needs to sit inside a closure or devtools gets mad)
@@ -74,8 +96,11 @@ fake_func <- function() {
     peer_percentile_pipe()
   
   head(bizz) %>% print.AsIs()
+
+  buzz <- all_assess_tidy  
   
-  
+  tidy_df <- all_assess_tidy %>%
+    dplyr::filter(testing_year %in% c(2014, 2013, 2012))
   
 }
 
