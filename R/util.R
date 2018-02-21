ac5671dc35998a82e3d66dbb82ff614e53acd19e
almartin82 <almartin@gmail.com>
2018-02-21 14:30:52 -0500

    cleaning code to make county, district, school code variable names consistent

--- R/util.R
+++ R/util.R
@@ -56,3 +56,56 @@ pad_grade <- function(x) {
 clean_id <- function(x) {
   gsub("(^|[^0-9])0+", "\\1", x, perl = TRUE)
 }
+
+
+#' Clean up CDS field names
+#'
+#' @param df 
+#'
+#' @return df, with consistent county_code, district_code, school_code fields
+#' @export
+
+clean_cds_fields <- function(df) {
+  names(df) <- gsub('co_code|co\\b', 'county_code', names(df))
+  names(df) <- gsub('dist_code|dist\\b', 'district_code', names(df))
+  names(df) <- gsub('sch_code|sch\\b', 'school_code', names(df))
+  names(df) <- gsub('yr\\b', 'year', names(df))
+  
+  df
+}
+
+
+#' report card year matcher
+#'
+#' @param df a report card table that includes trailing/longitudinal data
+#' @param end_year the correct year
+#'
+#' @return data frame with only data for the year of the report
+#' @export
+
+rc_year_matcher <- function(df) {
+  #convert the 0708 to 2008, etc
+  yy <- stringr::str_sub(df$year, 3, 4) %>% as.numeric()
+  df$yy <- ifelse(yy >= 68 %% 100, 1900+yy, 2000+yy)
+  
+  #filter on end year
+  df %>%
+    filter(yy == end_year) %>%
+    select(-yy)
+}
+
+
+clean_name_vector <- . %>%
+  gsub("'", "", .) %>% 
+  gsub("\"", "", .) %>% 
+  gsub("%", ".percent_", .) %>% 
+  gsub("#", ".number_", .) %>% 
+  gsub('-', '_', .) %>%
+  gsub("^[[:space:][:punct:]]+", "", .) %>% 
+  make.names(.) %>% 
+  snakecase::to_any_case(
+    case = 'snake', 
+    sep_in = "\\.",
+    transliterations = c("Latin-ASCII"), parsing_option = 4
+  )
+
