3fcb6b7ce9b3b7bfe2a2c71d520e1759ece3bf5f
almartin82 <almartin@gmail.com>
2018-02-23 12:33:05 -0500

    tests for rc functions

---
+++ tests/testthat/test_report_card.R
@@ -0,0 +1,28 @@
+context("report_card functions")
+
+rc <- get_one_rc_database(2015)
+many_rc <- get_rc_databases(2012:2017)
+
+test_that("get_raw_rc_database returns list of data frames", {
+  
+  expect_is(rc, 'list')
+  expect_equal(length(rc), 25)
+  expect_equal(
+    names(rc),
+    c("sch_header", "lang", "enrollment", "esea_waiver", "parcc_ela", 
+      "parcc_math", "parcc_state_avg", "njask_science", "biology", 
+      "naep", "ap_ib", "ap_ib_sum", "participation_pct_sat_psat_act", 
+      "sat_avg", "sat_1550", "algebra", "chronic_absenteeism", "graduation", 
+      "graduation_pathways", "post_sec", "dropout", 
+      "visual_arts", "cte_sle_dual_enr", "sgp", "school_climate"
+    )
+  )
+})
+
+
+test_that("get_raw_rc_database returns list of data frames", {
+  
+  expect_is(many_rc, 'list')
+  expect_equal(length(many_rc), 6)
+  
+})
