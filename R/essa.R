0bdfcf2b42fc7c8b39486cb009657fb45f443a29
almartin82 <almartin@gmail.com>
2018-03-10 20:40:22 -0500

    comprehensive or targeted in a single file

--- R/essa.R
+++ R/essa.R
@@ -1,18 +1,21 @@
-#' Get the ESSA comprehensive file
+#' Get an ESSA comprehensive or targeted accountability file
 #'
 #' @param end_year a school year.  end_year is the end of the academic year - eg 2016-17
 #' school year is end_year 2017.  valid values are 2017
+#' @param file_type 'comprehensive' or 'targeted'? 
 #'
 #' @return list of data frames
 #' @export
 
-get_essa_comprehensive_file <- function(end_year) {
+get_essa_file <- function(end_year, file_type = 'comprehensive') {
   essa_urls <- list(
-    "2017" = "http://www.state.nj.us/education/title1/accountability/progress/17/public_comprehensivefile.xlsx"
+    "comprehensive_2017" = "http://www.state.nj.us/education/title1/accountability/progress/17/public_comprehensivefile.xlsx",
+    "targeted_2017" = "http://www.state.nj.us/education/title1/accountability/progress/17/public_targetfile.xlsx"
   )
-  tmp_essa = tempfile(fileext = '.xlsx')
+  target_url <- essa_urls[[paste(file_type, as.character(end_year), sep = '_')]]
   
-  download.file(url = essa_urls[[as.character(end_year)]], destfile = tmp_essa, mode = "wb")
+  tmp_essa = tempfile(fileext = '.xlsx')
+  download.file(url = target_url, destfile = tmp_essa, mode = "wb")
   
   sheets_pr <- readxl::excel_sheets(tmp_essa) %>%
     clean_name_vector()
