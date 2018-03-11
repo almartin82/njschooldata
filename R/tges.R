9024968ba3487e9b02d3d07a2f9e3b7adb27077d
almartin82 <almartin@gmail.com>
2018-03-11 15:39:39 -0400

    indicator 20

--- R/tges.R
+++ R/tges.R
@@ -294,6 +294,42 @@ tidy_generic_personnel <- function(df, end_year, indicator) {
 }
 
 
+#' tidy budgeted vs actual fund balance
+#'
+#' @param df general fund vs actual used data frame, eg CSG20 
+#' output from get_raw_tges()
+#' @param end_year end year that the report was published
+#'
+#' @return data frame
+#' @export
+
+tidy_budgeted_vs_actual_fund_balance <- function(df, end_year) {
+  df$indicator <- 'Budgeted General Fund Balance vs. Actual'
+  
+  y1_df <- df[, c('group', 'county_name', 'district_code', 'district_name',
+                   'de120', 'de220', 'indicator')]
+  y2_df <- df[, c('group', 'county_name', 'district_code', 'district_name',
+                   'de320', 'de420', 'indicator')]
+  
+  indicator_fields <- list(
+    'de120' = 'Budgeted General Fund Balance',
+    'de220' = 'Actual',
+    'de320' = 'Budgeted General Fund Balance',
+    'de420' = 'Actual'
+  )
+  
+  y1_df$end_year <- end_year - 2
+  y1_df$report_year <- end_year
+  names(y1_df) <- tges_name_cleaner(y1_df, indicator_fields)
+  
+  y2_df$end_year <- end_year - 1
+  y2_df$report_year <- end_year
+  names(y2_df) <- tges_name_cleaner(y2_df, indicator_fields)
+  
+  bind_rows(y1_df, y2_df)
+}
+
+
 #' Tidy Budgetary Per Pupil data frame
 #'
 #' @param df indicator data frame, eg output of get_raw_tges()
@@ -566,7 +602,8 @@ tidy_tges_data <- function(list_of_dfs, end_year) {
     "CSG16" = "tidy_ratio_students_to_teachers",
     "CSG17" = "tidy_ratio_students_to_special_service",
     "CSG18" = "tidy_ratio_students_to_administrators",
-    "CSG19" = "tidy_ratio_faculty_to_administrators"
+    "CSG19" = "tidy_ratio_faculty_to_administrators",
+    "CSG20" = "tidy_budgeted_vs_actual_fund_balance"
   )
   
   #apply a cleaning function if known
