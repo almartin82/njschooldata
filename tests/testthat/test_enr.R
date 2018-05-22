7d439fe5a6e2911f843a38e7355d1a79b8f7dd5b
almartin82 <almartin@nps.k12.nj.us>
2018-05-22 17:22:43 -0400

    new tests for current enrollment data

--- tests/testthat/test_enr.R
+++ tests/testthat/test_enr.R
@@ -3,7 +3,7 @@ context("functions in enr")
 #post-NJSMART
 test_that("get_raw_enr correctly grabs the 2015 enrollment file", {
   ex_2015 <- get_raw_enr(2015)
-  
+
   expect_equal(nrow(ex_2015), 26223)
   expect_equal(ncol(ex_2015), 28)
   expect_equal(sum(ex_2015$ROW_TOTAL, na.rm = TRUE), 10954804)
@@ -12,7 +12,7 @@ test_that("get_raw_enr correctly grabs the 2015 enrollment file", {
 
 test_that("get_raw_enr correctly grabs the 2011 enrollment file", {
   ex_2011 <- get_raw_enr(2011)
-  
+
   expect_equal(nrow(ex_2011), 25891)
   expect_equal(ncol(ex_2011), 27)
   expect_equal(sum(ex_2011$ROW_TOTAL, na.rm = TRUE), 10871910)
@@ -22,7 +22,7 @@ test_that("get_raw_enr correctly grabs the 2011 enrollment file", {
 #pre-NJSMART
 test_that("get_raw_enr correctly grabs the 2010 enrollment file", {
   ex_2010 <- get_raw_enr(2010)
-  
+
   expect_equal(nrow(ex_2010), 29599)
   expect_equal(ncol(ex_2010), 29)
   expect_equal(sum(ex_2010$ROW_TOTAL, na.rm = TRUE), 11084504)
@@ -31,7 +31,7 @@ test_that("get_raw_enr correctly grabs the 2010 enrollment file", {
 
 test_that("get_raw_enr correctly grabs the 2001 enrollment file", {
   ex_2001 <- get_raw_enr(2001)
-  
+
   expect_equal(nrow(ex_2001), 28447)
   expect_equal(ncol(ex_2001), 20)
   expect_equal(sum(ex_2001$ROWTOTAL, na.rm = TRUE), 10522596)
@@ -40,8 +40,39 @@ test_that("get_raw_enr correctly grabs the 2001 enrollment file", {
 
 test_that("fetch_enr correctly grabs the 2012 enrollment file", {
   fetch_2009 <- fetch_enr(2009)
-  
+
   expect_equal(nrow(fetch_2009), 29491)
   expect_equal(ncol(fetch_2009), 29)
   expect_equal(sum(as.numeric(fetch_2009$row_total), na.rm = TRUE), 11034082)
 })
+
+
+test_that("fetch_enr handles the 2016-17 enrollment file", {
+  fetch_2017 <- fetch_enr(2017)
+
+  expect_equal(nrow(fetch_2009), 26470)
+  expect_equal(ncol(fetch_2009), 26)
+})
+
+
+test_that("fetch_enr handles the 2017-18 enrollment file", {
+  fetch_2018 <- fetch_enr(2018)
+
+  expect_is(fetch_2018, 'data.frame')
+  expect_equal(nrow(fetch_2009), 26470)
+  expect_equal(ncol(fetch_2009), 26)
+})
+
+
+test_that("all enrollment data can be pulled", {
+  enr_all <- map_df(
+    c(1999:2018),
+    fetch_enr
+  )
+
+  expect_is(enr_all, 'data.frame')
+  expect_equal(nrow(enr_all), 558627)
+  expect_equal(ncol(enr_all), 33)
+})
+
+
