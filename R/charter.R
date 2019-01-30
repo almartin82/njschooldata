863b26be0e26764ad376e53d95b2df366c6d02bd
almartin82 <almartin@gmail.com>
2019-01-30 08:17:29 -0500

    use district_code or district_id

--- R/charter.R
+++ R/charter.R
@@ -7,12 +7,18 @@
 
 id_charter_hosts <- function(df) {
   ensure_that(
-    df, 'district_id' %in% names(.) ~ 
-      "supplied dataframe must contain a column called ''district_id'"
+    df, 'district_id' %in% names(.) | 'district_code' %in% names(.) ~ 
+      "supplied dataframe must contain 'district_id' or 'district_code'"
   )
   
-  df_new <- df %>%
-    left_join(charter_city %>% select(-district_name), by = 'district_id')
+  charter_city_slim <- charter_city %>% select(-district_name)
+  
+  if ('district_id' %in% names(df)) {
+    df_new <- df %>% left_join(charter_city_slim, by = 'district_id')
+  } else if ('district_code' %in% names(df)) {
+    names(charter_city_slim)[1] <- 'district_code'
+    df_new <- df %>% left_join(charter_city_slim, by = 'district_code')
+  }
 
   ensure_that(
     df, nrow(.) == nrow(df_new) ~ 'joining to the charter hosts data set changed the size of your input dataframe.  this could be an issue with the `charter_city` dataframe included in this package.'
