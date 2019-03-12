67e6e7649819340f4607a47a7720bb6cd7fe57d7
almartin82 <almartin@gmail.com>
2019-03-12 19:35:27 -0400

    friendly school names

--- R/lookups.R
+++ R/lookups.R
@@ -37,7 +37,7 @@ friendly_district_names <- function(df) {
 }
 
 
-#' Given friendly names, returns ids
+#' Given friendly district names, returns ids
 #'
 #' @param district_names vector or list of friendly district names
 #' @param df df used to generate the friendly names
@@ -46,10 +46,66 @@ friendly_district_names <- function(df) {
 #' @export
 
 district_name_to_id <- function(district_names, df) {
-  df %>% 
+  out <- df %>% 
     filter(district_name %in% district_names) %>%
     pull(district_id) %>%
     unique()
+  # TODO: what about the collisions?
+  
+  out
 }
 
 
+#' Friendly School Names
+#'
+#' @param df data.frame that contains school_id and school_name
+#' @return character vector of names, one per school id
+#' @export
+
+friendly_school_names <- function(df) {
+  # group by district id, tag by most recent year in df, take first
+  df_first <- df %>% 
+    group_by(school_id) %>% 
+    arrange(-end_year) %>%
+    mutate(count = row_number()) %>%
+    ungroup() %>%
+    filter(count == 1)
+  
+  # count unique districts per district_name
+  name_count <- df_first %>%
+    group_by(school_name) %>%
+    summarize(
+      n = n()
+    )
+  
+  # if n > 1 put in district
+  df_first <- df_first %>%
+    left_join(name_count, by='school_name') %>%
+    mutate(
+      school_name = ifelse(
+        n > 1, 
+        paste0(school_name, ' (', district_name, ')'),
+        school_name
+      )
+    )
+  
+  df_first$school_name %>% sort()
+}
+
+
+#' Given friendly school names, returns ids
+#'
+#' @param school_names vector or list of friendly school names
+#' @param df df used to generate the friendly names
+#'
+#' @return vector of school_ids
+#' @export
+
+school_name_to_id <- function(school_names, df) {
+  out <- df %>% 
+    filter(school_name %in% school_names) %>%
+    pull(school_id) %>%
+    unique()
+  
+  out
+}
