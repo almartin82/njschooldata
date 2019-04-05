5fb028656a7ba75ff6d35ffa919a4fc0cc952649
almartin82 <almartin@gmail.com>
2019-04-05 07:27:30 -0400

    parcc aggregation function, documentation

--- R/parcc_calcs.R
+++ R/parcc_calcs.R
@@ -56,3 +56,67 @@ parcc_aggregate_calcs <- function(df) {
       schools = schools
     )
 }
+
+
+
+#' Aggregate PARCC results across multiple grade levels
+#' 
+#' @description a wrapper around fetch_parcc and parcc_aggregate_calcs that simplifies
+#' the calculation of multi-grade PARCC aggregations
+#' 
+#' @param end_year school year / testing year
+#' @param subj one of 'ela' or 'math'
+#' @param k8 boolean, if TRUE will calculate for K-8 only.
+#'
+#' @return dataframe
+#' @export
+
+calculate_agg_parcc_prof <- function(end_year, subj, k8=FALSE) {
+  
+  # grade and subjects to map over
+  if (subj == 'ela' & !k8) {
+    grades <- c(3:11)
+  } else if (subj == 'ela' & k8) {
+    grades <- c(3:8)
+  } else if (subj == 'math' & !k8) {
+    grades <- c('03', '04', '05', '06', '07', '08', 'ALG1', 'GEO', 'ALG2')
+  } else if (subj == 'math' & k8) {
+    grades <- c(3:8)
+  } else {
+    stop('invalid subject')
+  }
+  
+  # get relevant PARCC files
+  all_grades <- map_df(
+    grades,
+    function(x) fetch_parcc(
+      end_year = end_year, 
+      grade_or_subj = x, 
+      subj = subj, 
+      tidy = TRUE
+    )
+  )
+  
+  # get the counts from percentages
+  all_grades <- parcc_perf_level_counts(all_grades)
+  
+  # group, aggregate, return
+  all_grades %>%
+    filter(!is.na(county_code)) %>%
+    group_by(
+      county_code, county_name,
+      district_code, district_name,
+      school_code, school_name,
+      dfg, 
+      subgroup,
+      subgroup_type,
+      testing_year,
+      assess_name,
+      district_school
+    ) %>%
+    parcc_aggregate_calcs() %>%
+    ungroup() %>%
+    mutate(
+      grade = ifelse(k8, 'K-8', 'K-11')
+    )
+}
