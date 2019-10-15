5d1dc0b2fc95bc811a59bc0f433c7e7bc435eb22
almartin82 <almartin@gmail.com>
2019-10-14 21:26:28 -0400

    grad rate and count aggregat logic

--- R/grate_calcs.R
+++ R/grate_calcs.R
@@ -1,22 +1,3 @@
-#' PARCC counts by performance level
-#'
-#' @param df dataframe, output of fetch_parcc
-#'
-#' @return df with counts of students by performance level
-#' @export
-
-parcc_perf_level_counts <- function(df) {
-  df %>%
-    dplyr::mutate(
-      num_l1 = round((pct_l1/100) * number_of_valid_scale_scores, 0),
-      num_l2 = round((pct_l2/100) * number_of_valid_scale_scores, 0),
-      num_l3 = round((pct_l3/100) * number_of_valid_scale_scores, 0),
-      num_l4 = round((pct_l4/100) * number_of_valid_scale_scores, 0),
-      num_l5 = round((pct_l5/100) * number_of_valid_scale_scores, 0)
-    )  
-}
-
-
 #' Aggregate multiple grad rate rows and produce summary statistics
 #'
 #' @param df grouped df of grate data
@@ -43,4 +24,24 @@ grate_aggregate_calcs <- function(df) {
       districts = districts,
       schools = schools
     )
+}
+
+
+#' Aggregate multiple grad count rows and produce summary statistics
+#'
+#' @param df grouped df of gcount data
+#'
+#' @return df with aggregate stats for whatever grouping was provided
+#' @export
+
+gcount_aggregate_calcs <- function(df) {
+  df %>%
+    dplyr::summarize(
+      cohort_count = sum(cohort_count, na.rm = TRUE),
+      graduated_count = sum(graduated_count, na.rm = TRUE),
+      
+      districts = toString(district_name),
+      schools = toString(school_name),
+      n_charter_rows = sum(is_charter, na.rm = TRUE)
+    ) 
 }
