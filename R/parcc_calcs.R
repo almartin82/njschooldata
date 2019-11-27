ba9c4602e6781dadeea55633fb3ff7ea31215276
almartin82 <almartin@gmail.com>
2019-11-26 22:13:04 -0500

    njsla no ela 11

--- R/parcc_calcs.R
+++ R/parcc_calcs.R
@@ -80,11 +80,13 @@ calculate_agg_parcc_prof <- function(end_year, subj, gradespan='3-11') {
   # grade and subjects to map over
   if (subj == 'ela' & gradespan == '3-11' & end_year >= 2019) {
     grades <- c(3:10)
-  } else if (subj == 'ela' & gradespan == '3-11' & end_year >= 2019) {
+  } else if (subj == 'ela' & gradespan == '3-11' & end_year < 2019) {
     grades <- c(3:11)
   } else if (subj == 'ela' & gradespan == '3-8') {
     grades <- c(3:8)
-  } else if (subj == 'ela' & gradespan == '9-11') {
+  } else if (subj == 'ela' & gradespan == '9-11' & end_year >= 2019) {
+    grades <- c(9:10)
+  } else if (subj == 'ela' & gradespan == '9-11' & end_year < 2019) {
     grades <- c(9:11)
     
   } else if (subj == 'math' & gradespan == '3-11') {
