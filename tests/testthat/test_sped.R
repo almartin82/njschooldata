353a92f325180f72191fdf5f1f6a2c35eced2b04
almartin82 <almartin@gmail.com>
2020-05-12 23:04:02 -0400

    tests on raw sped and fetch sped with ground truth atlantic city and east orange

--- tests/testthat/test_sped.R
+++ tests/testthat/test_sped.R
@@ -11,8 +11,27 @@ test_that("get raw sped works across all years", {
 })
 
 
-
 test_that("fetch sped works across all years", {
+  
+  fetch_sped_df <- map(
+    all_end_years, ~fetch_sped(.x)
+  )
+  expect_is(fetch_sped_df, 'list')
+})
+
+
+test_that("raw sped returns county names and ids correctly", {
+  
+  target_years <- c(2003:2008)
+  
+  missing_ids <- map_df(
+    target_years, ~fetch_sped(.x)
+  )
+  expect_is(fetch_sped_df, 'tbl_df')
+})
+
+
+test_that("fetch sped works across all years and binds into dataframe", {
   
   fetch_sped_df <- map_df(
     all_end_years, ~fetch_sped(.x)
@@ -21,14 +40,20 @@ test_that("fetch sped works across all years", {
 })
 
 
-
-test_that("raw sped returns county names and ids correctly", {
+test_that("ground truth numbers on end year 2011 SPED data", {
   
-  target_years <- c(2003:2008)
+  ex_file = fetch_sped(2011)
 
-  missing_ids <- map_df(
-    target_years, ~fetch_sped(.x)
-  )
+  atlantic_city = ex_file %>%
+    filter(district_id == '0110')
+  expect_equal(atlantic_city$gened_num, 6821)
+  expect_equal(atlantic_city$sped_num, 1017)
+  expect_equal(atlantic_city$sped_rate, 14.90984, tolerance = .0001)
 
-  
+  eo = ex_file %>%
+    filter(district_id == '6410')
+  expect_equal(eo$gened_num, 470)
+  expect_equal(eo$sped_num, 0)
+  expect_equal(eo$sped_rate, 0, tolerance = .0001)
 })
+
