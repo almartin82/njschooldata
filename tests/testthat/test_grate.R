938a0bda379f016890ecbbc788db4ab555eff7eb
chiouey <31549973+chiouey@users.noreply.github.com>
2020-05-25 23:31:40 -0400

    add 2019 4y grad rate and 2018 5y grad rate
    - add new groups to grad_file_group_cleanup; 2019 combines asian and pacific islander
    - format get_raw_grad_file logic
    - add new urls for 2019 4y and 2018 5y
    - add 5y grad rate column names to tidy_new_format()
    - divide 5y grad rates in years 2012, 15-18

--- tests/testthat/test_grate.R
+++ tests/testthat/test_grate.R
@@ -5,13 +5,53 @@ grad_rate_cols <- c(
   "county_id", "county_name", 
   "district_id", "district_name", 
   "school_id", "school_name",
-  "group", 
+  "subgroup", 
   "grad_rate", 
   "cohort_count", "graduated_count", 
-  "methodology"
+  "methodology", 
+  "is_state", "is_county", "is_district", "is_school",
+  "is_charter", "is_charter_sector", "is_allpublic"
 )
 
-test_that('fetch grate works properly', {
+
+## 4 year
+test_that('fetch_grad_rate works with 4 year', {
+   ex0 <- fetch_grad_rate(2012, '4 year')
+   ex1 <- fetch_grad_rate(2013, '4 year')
+   ex2 <- fetch_grad_rate(2014, '4 year')
+   ex3 <- fetch_grad_rate(2015, '4 year')
+   ex4 <- fetch_grad_rate(2016, '4 year')
+   ex5 <- fetch_grad_rate(2017, '4 year')
+   ex6 <- fetch_grad_rate(2018, '4 year')
+   ex7 <- fetch_grad_rate(2019, '4 year')
+   
+   expect_is(ex0, 'data.frame')
+   expect_is(ex1, 'data.frame')
+   expect_is(ex2, 'data.frame')
+   expect_is(ex3, 'data.frame')
+   expect_is(ex4, 'data.frame')
+   expect_is(ex5, 'data.frame')
+   expect_is(ex6, 'data.frame')
+   expect_is(ex7, 'data.frame')
+})
+
+test_that('fetch_grad_rate all years', {
+   
+   expect_error(fetch_grad_rate(2010))
+   grr12 <- fetch_grad_rate(2011)
+   grr13 <- fetch_grad_rate(2012)
+   grr14 <- fetch_grad_rate(2013)
+   grr15 <- fetch_grad_rate(2014)
+   grr16 <- fetch_grad_rate(2015)
+   grr17 <- fetch_grad_rate(2016)
+   grr18 <- fetch_grad_rate(2017)
+   grr19 <- fetch_grad_rate(2018)
+   grr20 <- fetch_grad_rate(2019)
+   expect_error(fetch_grad_rate(2020))
+   
+})
+
+test_that('fetch grate works with 2015 data', {
   ex <- fetch_grad_rate(2015)
   expect_is(ex, 'data.frame')
   expect_equal(sum(ex$graduated_count, na.rm = TRUE), 674621)
@@ -19,18 +59,44 @@ test_that('fetch grate works properly', {
 })
 
 
-test_that('fetch grate works with more recent data', {
+test_that('fetch grate works with 2018 data', {
   ex <- fetch_grad_rate(2018)
   expect_is(ex, 'data.frame')
   expect_equal(sum(ex$graduated_count, na.rm = TRUE), 996902)
   expect_equal(names(ex), grad_rate_cols)
 })
 
+test_that("ground truth values on 2019 grate", {
+   ex <- fetch_grad_rate(2019)
+   expect_is(ex, "data.frame")
+   expect_equal(names(ex), grad_rate_cols)
+   
+   expect_equal(filter(ex, 
+                       district_id == '3570',
+                       school_id == '030',
+                       subgroup == 'black') %>%
+                   pull(grad_rate), .744)
+   
+   expect_equal(filter(ex, 
+                       district_id == '3570',
+                       school_id == '030',
+                       subgroup == 'white') %>%
+                   pull(grad_rate), NA_real_)
+   
+   expect_equal(filter(ex, 
+                       district_id == '3570',
+                       school_id == '030',
+                       subgroup == 'students with disability') %>%
+                   pull(grad_rate), .594)
+})
 
+
+
+## 5 year
 test_that('five year window is in more recent data', {
-  ex <- fetch_grad_rate(2017, '5 year')
-  expect_is(ex, 'data.frame')
-  expect_equal(names(ex), grad_rate_cols)
+   ex <- fetch_grad_rate(2017, '5 year')
+   expect_is(ex, 'data.frame')
+   expect_equal(names(ex), grad_rate_cols)
 })
 
 
@@ -41,6 +107,7 @@ test_that('get_raw_grate works with 5 year', {
   ex3 <- get_grad_rate(2015, '5 year')
   ex4 <- get_grad_rate(2016, '5 year')
   ex5 <- get_grad_rate(2017, '5 year')
+  ex6 <- get_grad_rate(2018, '5 year')
   
   expect_is(ex0, 'data.frame')
   expect_is(ex1, 'data.frame')
@@ -48,6 +115,7 @@ test_that('get_raw_grate works with 5 year', {
   expect_is(ex3, 'data.frame')
   expect_is(ex4, 'data.frame')
   expect_is(ex5, 'data.frame')
+  expect_is(ex6, 'data.frame')
 })
 
 
@@ -58,24 +126,7 @@ test_that('fetch_grad_rate works with 5 year', {
   ex3 <- fetch_grad_rate(2015, '5 year')
   ex4 <- fetch_grad_rate(2016, '5 year')
   ex5 <- fetch_grad_rate(2017, '5 year')
-  
-  expect_is(ex0, 'data.frame')
-  expect_is(ex1, 'data.frame')
-  expect_is(ex2, 'data.frame')
-  expect_is(ex3, 'data.frame')
-  expect_is(ex4, 'data.frame')
-  expect_is(ex5, 'data.frame')
-})
-
-
-test_that('fetch_grad_rate works with 4 year', {
-  ex0 <- fetch_grad_rate(2012, '4 year')
-  ex1 <- fetch_grad_rate(2013, '4 year')
-  ex2 <- fetch_grad_rate(2014, '4 year')
-  ex3 <- fetch_grad_rate(2015, '4 year')
-  ex4 <- fetch_grad_rate(2016, '4 year')
-  ex5 <- fetch_grad_rate(2017, '4 year')
-  ex6 <- fetch_grad_rate(2018, '4 year')
+  ex6 <- fetch_grad_rate(2018, '5 year')
   
   expect_is(ex0, 'data.frame')
   expect_is(ex1, 'data.frame')
@@ -84,10 +135,33 @@ test_that('fetch_grad_rate works with 4 year', {
   expect_is(ex4, 'data.frame')
   expect_is(ex5, 'data.frame')
   expect_is(ex6, 'data.frame')
-  
 })
 
 
+test_that("ground truth values on 2018 5y grate", {
+   ex <- fetch_grad_rate(2018, '5 year')
+   expect_is(ex, "data.frame")
+   
+   expect_equal(filter(ex, 
+                       district_id == '3570',
+                       school_id == '888') %>%
+                   pull(grad_rate), .765)
+   
+   expect_equal(filter(ex, 
+                       district_id == '3570',
+                       school_id == '030') %>%
+                   pull(grad_rate), .798)
+   
+   expect_equal(filter(ex, 
+                       district_id == '3570',
+                       school_id == '307') %>%
+                   pull(grad_rate), NA_real_)
+})
+
+
+
+
+# grad count
 test_that('fetch_grad_count all years', {
   
   gr00 <- fetch_grad_count(1999)
@@ -110,23 +184,9 @@ test_that('fetch_grad_count all years', {
   gr17 <- fetch_grad_count(2016)
   gr18 <- fetch_grad_count(2017)
   gr19 <- fetch_grad_count(2018)
-  expect_error(fetch_grad_count(2019))
+  expect_error(fetch_grad_count(2020))
   
 })
 
 
 
-test_that('fetch_grad_rate all years', {
-  
-  expect_error(fetch_grad_rate(2010))
-  grr12 <- fetch_grad_rate(2011)
-  grr13 <- fetch_grad_rate(2012)
-  grr14 <- fetch_grad_rate(2013)
-  grr15 <- fetch_grad_rate(2014)
-  grr16 <- fetch_grad_rate(2015)
-  grr17 <- fetch_grad_rate(2016)
-  grr18 <- fetch_grad_rate(2017)
-  grr19 <- fetch_grad_rate(2018)
-  expect_error(fetch_grad_rate(2019))
-  
-})
