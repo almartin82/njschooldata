97faa9c9d65d481a9acdbe38807780f43cf07a0a
chiouey <31549973+chiouey@users.noreply.github.com>
2020-05-31 09:56:26 -0400

    add geographic aggregates to parcc data
    - ungroup input to enrich_school_city_ward
      - fetch_all_parcc output is grouped
    - create ward_parcc_aggs in the style of ward_enr_aggs
    - write return type and ground truth tests for ward_parcc_aggs

--- tests/testthat/test_geo.R
+++ tests/testthat/test_geo.R
@@ -15,3 +15,39 @@ test_that("ward_enr_aggs correctly aggregates newark enrollment data by ward", {
   enr_2019_wards <- ward_enr_aggs(enr_2019)
   testthat::expect_is(enr_2019_wards, 'data.frame')
 })
+
+
+
+# PARCC
+parcc_ela_8_2019 <- fetch_parcc(end_year = 2019, grade_or_subj = 8, 
+                          subj = 'ela' ,tidy = T)
+
+test_that("ward_parcc_aggs correctly aggregates newark parcc data by ward" , {
+   parcc_ela_8_2019 %>%
+      ward_parcc_aggs() %>%
+      testthat::expect_is('data.frame')
+})
+
+test_that("ground truth values for parcc ward aggregations", {
+   parcc_ela8_19_ward <- parcc_ela_8_2019 %>%
+      ward_parcc_aggs()
+   
+   expect_equal(parcc_ela8_19_ward %>%
+                   filter(subgroup == "special_education",
+                          district_name == "Newark City CENTRAL") %>%
+                   pull(number_of_valid_scale_scores),
+                54)
+   
+   expect_equal(parcc_ela8_19_ward %>%
+                   filter(subgroup == "asian",
+                          district_name == "Newark City NORTH") %>%
+                   pull(n_schools),
+                2)
+   
+   expect_equal(parcc_ela8_19_ward %>%
+                   filter(subgroup == "lep_current",
+                          district_name == "Newark City EAST") %>%
+                   pull(scale_score_mean) %>%
+                   round(1),
+                725.2)
+})
