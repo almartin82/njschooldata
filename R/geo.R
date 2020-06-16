13189005b12520bcce366f9f7f15609b11cd1406
almartin82 <almartin@gmail.com>
2020-06-15 22:45:52 -0400

    use existing parcc aggregation code

--- R/geo.R
+++ R/geo.R
@@ -1,6 +1,6 @@
 #' Enrich School Data with Lat / Long
 #'
-#' @param df dataframe to be enriched 
+#' @param df dataframe to be enriched
 #' @param use_cache if TRUE, will read from cache of school info / lat lng stored on TODO
 #' @param api_key optional, personal google maps API key
 #'
@@ -8,16 +8,16 @@
 #' @export
 
 enrich_school_latlong <- function(df, use_cache=TRUE, api_key='') {
-  
+
   kill_padformulas <- function(x) {
     gsub('="', '', x, fixed=TRUE) %>%
       gsub('"', "", ., fixed=TRUE)
   }
 
   # download and clean
-  nj_sch <- httr::GET('https://homeroom5.doe.state.nj.us/directory/schoolDL.php') %>% 
+  nj_sch <- httr::GET('https://homeroom5.doe.state.nj.us/directory/schoolDL.php') %>%
     httr::content(as="text") %>%
-    readr::read_csv(skip=3) %>% 
+    readr::read_csv(skip=3) %>%
     rename(
       district_id = `District Code`,
       school_id = `School Code`
@@ -27,7 +27,7 @@ enrich_school_latlong <- function(df, use_cache=TRUE, api_key='') {
       district_id,
       school_id,
       address1,
-      city, 
+      city,
       state,
       zip
     ) %>%
@@ -38,20 +38,20 @@ enrich_school_latlong <- function(df, use_cache=TRUE, api_key='') {
       address = paste0(address1, ', ', city, ', ', state, ' ', zip, ' USA'),
       address = gsub("\\s+", ' ', address)
     )
-  
+
   # geocode
   if (use_cache) {
     data("geocoded_cached")
   } else {
     geocoded <- placement::geocode_url(
-      nj_sch$address, 
-      auth='standard_api', 
-      privkey=api_key, 
-      clean=TRUE, 
+      nj_sch$address,
+      auth='standard_api',
+      privkey=api_key,
+      clean=TRUE,
       verbose=TRUE
     )
   }
-  
+
   geocoded_merge <- geocoded %>%
     select(locations, lat, lng) %>%
     rename(
@@ -76,32 +76,32 @@ enrich_school_latlong <- function(df, use_cache=TRUE, api_key='') {
 
 enrich_school_city_ward <- function(df) {
   supported_geos <- c('3570')
-  
+
   # say what fraction of the rows are supported
   supported <- df$district_id %in% supported_geos
   pct_supported <- supported %>%
     mean() %>%
     multiply_by(100) %>%
     round(1)
-  
+
   message(
-    paste0('ward information available for ', pct_supported, 
-           '% (', sum(supported), '/', length(supported), 
+    paste0('ward information available for ', pct_supported,
+           '% (', sum(supported), '/', length(supported),
            ') rows in this data set.')
   )
   # split into supported / unsupported
   geo_mask <- df$district_id %in% supported_geos
   latlong_mask <- !is.na(df$lat) & !is.na(df$lng)
   final_mask <- geo_mask & latlong_mask
-  
+
   df_supported <- df %>%
      ungroup() %>%
      filter(final_mask)
-    
+
   df_unsupported <- df %>%
      ungroup() %>%
      filter(!final_mask)
-  
+
   # add specific geos here
   # newark (3570)
   if ('3570' %in% df$district_id) {
@@ -112,7 +112,7 @@ enrich_school_city_ward <- function(df) {
     newark_wards$WARD_NAME <- as.character(newark_wards$WARD_NAME)
     sp::coordinates(df_supported) <- ~lng+lat
     sp::proj4string(df_supported) <- sp::proj4string(newark_wards)
-    
+
     df_supported$ward <- sp::over(df_supported, newark_wards)$WARD_NAME
     df_supported <- as_tibble(df_supported)
   }
@@ -122,15 +122,15 @@ enrich_school_city_ward <- function(df) {
 
 
 ward_enr_aggs <- function(df) {
-  
+
   # enrich
   df <- enrich_school_latlong(df) %>%
     enrich_school_city_ward()
-  
+
   df <- df %>%
     filter(!is.na(ward)) %>%
     group_by(
-      end_year, 
+      end_year,
       county_id, county_name,
       district_id, district_name,
       ward,
@@ -142,7 +142,7 @@ ward_enr_aggs <- function(df) {
       n_schools = n()
     ) %>%
     ungroup()
-  
+
   df <- df %>%
     mutate(
       CDS_Code = NA_character_,
@@ -160,10 +160,10 @@ ward_enr_aggs <- function(df) {
       is_subprogram = !program_code == '55'
     ) %>%
     select(-ward)
-  
+
   # calculate percent
   df <- agg_enr_pct_total(df)
-  
+
   # column order and return
   agg_enr_column_order(df)
 }
@@ -174,16 +174,15 @@ ward_enr_aggs <- function(df) {
 #' @param list_of_dfs output of \code{fetch_all_parcc} or \code{fetch_parcc}
 #'
 #' @return A data frame of ward aggregations
-#'
-#'
 #' @export
+
 ward_parcc_aggs <- function(list_of_dfs) {
-   
+
    df <- list_of_dfs %>%
       bind_rows() %>% # convert to df
       enrich_school_latlong() %>%
       enrich_school_city_ward()
-   
+
    df <- df %>%
       filter(!is.na(ward) #,
              #subgroup == "special_education",
@@ -201,40 +200,15 @@ ward_parcc_aggs <- function(list_of_dfs) {
          grade,
          subgroup, subgroup_type
       ) %>%
-      summarize(
-         n_schools = n(), 
-         number_enrolled = sum(number_enrolled, na.rm = T),
-         number_not_tested = sum(number_not_tested, na.rm = T),
-         scale_score_mean = sum(scale_score_mean * number_of_valid_scale_scores, na.rm = T) /
-                            sum(number_of_valid_scale_scores, na.rm = T),
-         number_of_valid_scale_scores = sum(number_of_valid_scale_scores, na.rm = T),
-         num_l1 = sum(num_l1, na.rm = T),
-         num_l2 = sum(num_l2, na.rm = T),
-         num_l3 = sum(num_l3, na.rm = T),
-         num_l4 = sum(num_l4, na.rm = T),
-         num_l5 = sum(num_l5, na.rm = T),
-         pct_l1 = 100 * num_l1 / sum(num_l1, num_l2, num_l3, num_l4, num_l5),
-         pct_l2 = 100 * num_l2 / sum(num_l1, num_l2, num_l3, num_l4, num_l5),
-         pct_l3 = 100 * num_l3 / sum(num_l1, num_l2, num_l3, num_l4, num_l5),
-         pct_l4 = 100 * num_l4 / sum(num_l1, num_l2, num_l3, num_l4, num_l5),
-         pct_l5 = 100 * num_l5 / sum(num_l1, num_l2, num_l3, num_l4, num_l5),
-         proficient_above = pct_l4 + pct_l5
-       ) %>%
-      ungroup()
-   
+     parcc_aggregate_calcs %>%
+     ungroup()
+
    df <- df %>%
       mutate(
          district_id = paste0(district_id, ' ', ward),
          district_name = paste0(district_name, ' ', ward),
          school_id = '999W',
          school_name = 'Ward Total',
-         number_of_valid_scale_scores = if_else(is.na(scale_score_mean), NA_real_, 
-                                                number_of_valid_scale_scores),
-         num_l1 = if_else(is.na(scale_score_mean), NA_real_, num_l1),
-         num_l2 = if_else(is.na(scale_score_mean), NA_real_, num_l2),
-         num_l3 = if_else(is.na(scale_score_mean), NA_real_, num_l3),
-         num_l4 = if_else(is.na(scale_score_mean), NA_real_, num_l4),
-         num_l5 = if_else(is.na(scale_score_mean), NA_real_, num_l5),
          is_state = FALSE,
          is_county = FALSE,
          is_citywide = FALSE,
@@ -246,7 +220,7 @@ ward_parcc_aggs <- function(list_of_dfs) {
          is_dfg = (county_id == 'DFG')
       ) %>%
       select(-ward)
-   
+
    return(df)
 }
 
--- tests/testthat/test_geo.R
+++ tests/testthat/test_geo.R
@@ -19,7 +19,7 @@ test_that("ward_enr_aggs correctly aggregates newark enrollment data by ward", {
 
 
 # PARCC
-parcc_ela_8_2019 <- fetch_parcc(end_year = 2019, grade_or_subj = 8, 
+parcc_ela_8_2019 <- fetch_parcc(end_year = 2019, grade_or_subj = 8,
                           subj = 'ela', tidy = T)
 
 test_that("ward_parcc_aggs correctly aggregates newark parcc data by ward" , {
@@ -31,19 +31,13 @@ test_that("ward_parcc_aggs correctly aggregates newark parcc data by ward" , {
 test_that("ground truth values for parcc ward aggregations", {
    parcc_ela8_19_ward <- parcc_ela_8_2019 %>%
       ward_parcc_aggs()
-   
+
    expect_equal(parcc_ela8_19_ward %>%
                    filter(subgroup == "special_education",
                           district_name == "Newark City CENTRAL") %>%
                    pull(number_of_valid_scale_scores),
                 54)
-   
-   expect_equal(parcc_ela8_19_ward %>%
-                   filter(subgroup == "asian",
-                          district_name == "Newark City NORTH") %>%
-                   pull(n_schools),
-                2)
-   
+
    expect_equal(parcc_ela8_19_ward %>%
                    filter(subgroup == "lep_current",
                           district_name == "Newark City EAST") %>%
@@ -53,9 +47,9 @@ test_that("ground truth values for parcc ward aggregations", {
 })
 
 test_that("grade aggregates work w/ ward aggregation", {
-   gr9_11_2018_mat <- calculate_agg_parcc_prof(end_year = 2018, 
+   gr9_11_2018_mat <- calculate_agg_parcc_prof(end_year = 2018,
                             subj = 'math',
-                            gradespan = '9-11') 
-   
+                            gradespan = '9-11')
+
       expect_is(gr9_11_2018_mat, 'data.frame')
 })
