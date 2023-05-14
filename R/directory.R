af65a8deb06153b2485174bb5eef3082f4e87e05
almartin82 <almartin@gmail.com>
2023-05-14 13:18:45 -0400

    namespace

--- R/directory.R
+++ R/directory.R
@@ -41,7 +41,7 @@ get_school_directory <- function() {
     readr::read_csv(skip = 3) %>%
     janitor::clean_names() %>%
     dplyr::mutate(
-      across(.fns = kill_padformulas)
+      across(.fns = njschooldata:::kill_padformulas)
     ) %>%
     dplyr::mutate(
       address = paste0(address1, ', ', city, ', ', state, ' ', zip)
